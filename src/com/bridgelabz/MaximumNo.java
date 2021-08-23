54287c0aac1fe7fa6d499d1a17112047226c026b
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-23 13:57:42 +0530

    ADDED : Integer Max

--- src/com/bridgelabz/MaximumNo.java
+++ src/com/bridgelabz/MaximumNo.java
@@ -4,5 +4,19 @@ public class MaximumNo {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Maximum number finding program");
+
+        MaximumNo maxNumber = new MaximumNo();
+        maxNumber.findMaximum(2, 4, 6);
+    }
+
+    public int findMaximum(Integer X, Integer Y, Integer Z) {
+        Integer max = X;
+        if (Y.compareTo(max) > 0)
+            max = Y;
+        if (Z.compareTo(max) > 0)
+            max = Z;
+        System.out.println("Maximum Number = " + max);
+
+        return max;
     }
 }
