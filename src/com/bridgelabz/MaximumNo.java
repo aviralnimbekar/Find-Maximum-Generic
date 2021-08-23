c22c68a433c66bb9d4a70e679927311ce518f9b8
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-23 14:26:04 +0530

    REFACTORED : All methods into one

--- src/com/bridgelabz/MaximumNo.java
+++ src/com/bridgelabz/MaximumNo.java
@@ -7,39 +7,17 @@ public class MaximumNo {
 
         MaximumNo maxNumber = new MaximumNo();
         maxNumber.findMaximum(2, 4, 6);
-        maxNumber.findMaximum(1.2f,1.4f,1.6f);
+        maxNumber.findMaximum(1.2f, 1.4f, 1.6f);
         maxNumber.findMaximum("Apple", "Peach", "Banana");
     }
 
-    public int findMaximum(Integer X, Integer Y, Integer Z) {
-        Integer max = X;
+    public <T extends Comparable> T findMaximum(T X, T Y, T Z) {
+        T max = X;
         if (Y.compareTo(max) > 0)
             max = Y;
         if (Z.compareTo(max) > 0)
             max = Z;
-        System.out.println("Maximum Number in Integer = " + max);
-
-        return max;
-    }
-
-    public Float findMaximum(Float X, Float Y, Float Z) {
-        Float max = X;
-        if (Y.compareTo(max) > 0)
-            max = Y;
-        if (Z.compareTo(max) > 0)
-            max = Z;
-        System.out.println("Maximum Number in float = " + max);
-
-        return max;
-    }
-
-    public String findMaximum(String X, String Y, String Z) {
-        String max = X;
-        if (Y.compareTo(max) > 0)
-            max = Y;
-        if (Z.compareTo(max) > 0)
-            max = Z;
-        System.out.println("Maximum Number in float = " + max);
+        System.out.println("Maximum Number = " + max);
 
         return max;
     }
