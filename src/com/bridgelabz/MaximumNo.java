ecb2b7c294323c0b944a3eb62318de0c757d87e8
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-23 15:58:27 +0530

    REFACTORED : made class generic

--- src/com/bridgelabz/MaximumNo.java
+++ src/com/bridgelabz/MaximumNo.java
@@ -1,24 +1,35 @@
 package com.bridgelabz;
 
-public class MaximumNo {
+public class MaximumNo<T extends Comparable> {
+
+    T X, Y, Z;
+
+    public MaximumNo(T x, T y, T z) {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public static <T extends Comparable> T findMaximum(T x, T y, T z) {
+        T max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+        if (z.compareTo(max) > 0)
+            max = z;
+        System.out.println("Maximum Number = " + max);
+
+        return max;
+    }
+
+    public void testMaximum() {
+        MaximumNo.findMaximum(X, Y, Z);
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Maximum number finding program");
 
-        MaximumNo maxNumber = new MaximumNo();
-        maxNumber.findMaximum(2, 4, 6);
-        maxNumber.findMaximum(1.2f, 1.4f, 1.6f);
-        maxNumber.findMaximum("Apple", "Peach", "Banana");
-    }
-
-    public <T extends Comparable> T findMaximum(T X, T Y, T Z) {
-        T max = X;
-        if (Y.compareTo(max) > 0)
-            max = Y;
-        if (Z.compareTo(max) > 0)
-            max = Z;
-        System.out.println("Maximum Number = " + max);
-
-        return max;
+        new MaximumNo<Integer>(2, 4, 6).testMaximum();
+        new MaximumNo<Float>(1.2f, 1.4f, 1.6f).testMaximum();
+        new MaximumNo<String>("Apple", "Peach", "Banana").testMaximum();
     }
 }
